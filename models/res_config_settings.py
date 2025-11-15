f9702798d92758e5ca5e6597361144e21ab58ebe
Bayuarga669 <bayuarga407@gmail.com>
2025-11-15 09:19:13 +0700

    fi

--- models/res_config_settings.py
+++ models/res_config_settings.py
@@ -7,6 +7,7 @@ class ResConfigSettings(models.TransientModel):
         string="Midtrans Server Key",
         config_parameter="payment_midtrans.server_key"
     )
+
     midtrans_client_key = fields.Char(
         string="Midtrans Client Key",
         config_parameter="payment_midtrans.client_key"
