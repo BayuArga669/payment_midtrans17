338a497adc86fa95eee5fa6b8251f323ac610b9d
Bayuarga669 <bayuarga407@gmail.com>
2025-11-15 09:22:48 +0700

    dwadwadw

--- models/res_config_settings.py
+++ models/res_config_settings.py
@@ -1,19 +1,30 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    midtrans_server_key = fields.Char(
-        string="Midtrans Server Key",
-        config_parameter="payment_midtrans.server_key"
-    )
+    midtrans_client_key = fields.Char("Midtrans Client Key")
+    midtrans_server_key = fields.Char("Midtrans Server Key")
+    midtrans_mode = fields.Selection([
+        ('sandbox', 'Sandbox'),
+        ('production', 'Production'),
+    ], default='sandbox')
 
-    midtrans_client_key = fields.Char(
-        string="Midtrans Client Key",
-        config_parameter="payment_midtrans.client_key"
-    )
+    def set_values(self):
+        super().set_values()
+        self.env['ir.config_parameter'].sudo().set_param(
+            'midtrans.client_key', self.midtrans_client_key)
+        self.env['ir.config_parameter'].sudo().set_param(
+            'midtrans.server_key', self.midtrans_server_key)
+        self.env['ir.config_parameter'].sudo().set_param(
+            'midtrans.mode', self.midtrans_mode)
 
-    midtrans_is_production = fields.Boolean(
-        string="Midtrans Production Mode",
-        config_parameter="payment_midtrans.production"
-    )
+    @api.model
+    def get_values(self):
+        res = super().get_values()
+        res.update(
+            midtrans_client_key=self.env['ir.config_parameter'].sudo().get_param('midtrans.client_key'),
+            midtrans_server_key=self.env['ir.config_parameter'].sudo().get_param('midtrans.server_key'),
+            midtrans_mode=self.env['ir.config_parameter'].sudo().get_param('midtrans.mode', 'sandbox'),
+        )
+        return res
